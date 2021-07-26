eafaaf24e317a0ff0ca2914078d0d7be9f4c4fd8
TeamMWOfficial <87971904+TeamMWOfficial@users.noreply.github.com>
2021-07-26 21:14:49 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -95,7 +95,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"📑 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 📑", callback_data="ignore")
+            InlineKeyboardButton(f"🔵Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔵", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<b>🗂️Total File :- {leng}</b>\n<b>🎬Movie Name :-</b> <code>{query}</code>"
+    text=f"<b>🎬Found Results For Your Movie:- {leng}</b>\n<b>⭐Movie Name :-</b> <code>{query}</code>"
         
     try:
         await update.message.edit(
